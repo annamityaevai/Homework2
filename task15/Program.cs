443bbf4d30a1471c0be8a58fdd9225473b69595b
Anna Mityaeva <aiisa@inbox.ru>
2022-12-01 08:52:44 +0300

    task 15

---
+++ task15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter weekday number");
+int day = Convert.ToInt32(Console.ReadLine());
+switch (day)
+{
+    case 1:
+        Console.WriteLine("no");
+        break;
+    case 2:
+        Console.WriteLine("no");
+        break;
+    case 3:
+        Console.WriteLine("no");
+        break;
+    case 4:
+        Console.WriteLine("no");
+        break;
+    case 5:
+        Console.WriteLine("no");
+        break;
+    case 6:
+        Console.WriteLine("yes");
+        break;
+    case 7:
+        Console.WriteLine("yes");
+        break;
+    default:
+        Console.WriteLine("No day");
+        break;
+}

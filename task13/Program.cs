c1dbd3585b3de49083c4e7f77c36a45ef0e9d3eb
Anna Mityaeva <aiisa@inbox.ru>
2022-12-01 08:40:22 +0300

    task 13

---
+++ task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Insert number");
+int number = Convert.ToInt32(Console.ReadLine());
+string array = Convert.ToString(number);
+if (array.Length > 2)
+{
+  Console.WriteLine("third digit = " + array[2]);
+}
+else 
+{
+  Console.WriteLine("no third digit");
+}
